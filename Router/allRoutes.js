70504e3390e27975f0b8ad4538eb76885b6b9cb7
Cedric <cedric.claire03@gmail.com>
2025-04-29 20:18:43 +0200

    Acceuil/connexion

--- Router/allRoutes.js
+++ Router/allRoutes.js
@@ -2,7 +2,15 @@ import Route from "./Route.js";
 
 //Définir ici vos routes
 export const allRoutes = [
-    new Route("/", "Accueil", "/pages/home.html"),];
+    new Route("/", "Accueil", "/pages/home.html"),
+    new Route("/itinéraires", "Les itinéraires", "/pages/itinéraires.html"),
+    new Route("/signin", "Connexion", "pages/auth/signin.html"),
+    new Route("/signup", "Inscription", "pages/auth/signup.html", "/js/auth/signup.js"),
+    new Route("/account", "Mon Compte", "pages/auth/account.html"),
+    new Route("/editPassword", "Changement de mot de passe", "pages/auth/editPassword.html"),
+    new Route("/allResa", "Vos Reservations", "pages/reservations/allResa.html"),
+    new Route("/reserver", "Réservez", "pages/reservations/reserver.html"),
+];
 
 //Le titre s'affiche comme ceci : Route.titre - websitename
 export const websiteName = "EcoRide";
---
+++ js/auth/signup.js
@@ -0,0 +1,54 @@
+// Implementer le js de ma page
+
+const inputNom = document.getElementById("NomInput");
+const inputPrenom = document.getElementById("PrenomInput");
+const inputMail = document.getElementById("EmailInput");
+const inputPassword = document.getElementById("PasswordInput");
+const inputValidationPassword = document.getElementById("ValidatePasswordInput");
+
+inputNom.addEventListener("keyup", validateForm);
+inputPrenom.addEventListener("keyup", validateForm);
+inputMail.addEventListener("keyup", validateForm);
+inputPassword.addEventListener("keyup", validateForm);
+inputValidationPassword.addEventListener("keyup", validateForm);
+
+function validateForm() {
+    validateRequired(inputNom);
+    validateRequired(inputPrenom);
+
+}
+
+function validateRequired(input) {
+    if (input.value != '') {
+        input.classList.add("is-valid");
+        input.classList.remove("is-invalid");
+    }
+
+    else {
+        input.classList.remove("is-invalid");
+        input.classList.add("is-invalid");
+    }
+
+}
+
+inputNom.addEventListener("keyup", validateForm);
+
+function validateForm() {
+    validateRequired(inputNom);
+    validateRequired(inputPrenom);
+
+}
+
+function validateRequired(input) {
+    if (input.value != '') {
+        input.classList.add("is-valid");
+        input.classList.remove("is-invalid");
+    }
+
+    else {
+        input.classList.remove("is-invalid");
+        input.classList.add("is-invalid");
+    }
+
+}
+
